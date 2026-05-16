96f4099044cee86824a3bbeef7e43b06137aa646
Manhar <smmanhar13@gmail.com>
2026-05-16 23:12:50 +0530

    Add GitHub Pages deploy workflow and configure base path

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import OrderSuccessPage from './pages/OrderSuccessPage';
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <Routes>
         <Route
           path="/"
